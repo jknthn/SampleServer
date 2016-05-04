02e05b1f130ea6db86743e324069c0f6278e2ea7
Jakub Tomanik <j.tomanik@gmail.com>
2016-05-04 19:26:18 +0200

    Updated kitura versions to support Swift DEVELOPMENT-25-04-2016

--- Package.swift
+++ Package.swift
@@ -25,14 +25,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 11),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 0, minor: 11),
+        .Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 8)
     ],
     exclude: ["Makefile", "Kitura-Build"])
 
